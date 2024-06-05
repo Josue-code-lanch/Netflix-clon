386196723d8c02026557dc8e77ff844da7513bc3
Josue Gonzalez <josuegr834@gmail.com>
2024-06-05 13:35:17 -0300

    Modificando los estilos del componente SectionInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Content from "./Components/Content";
 import SectionInfo from "./Components/SectionInfo";
+import SectionReverseInfo from "./Components/SectionReverseInfo";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,10 @@ function App() {
         text={
           "Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más."
         }
+      />
+      <SectionReverseInfo
+        title={"Descarga tus series para verlas offline"}
+        text={"Guarda tu contenido favorito y tendrás siempre algo para ver."}
       />
     </div>
   );
